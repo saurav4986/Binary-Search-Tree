6806a50362e94715655ab835af53730eaa680102
saurav4986 <saurav.raj4986@gmail.com>
2022-11-29 17:30:35 +0530

    Initial commit for Bst

---
+++ src/com/bridgelabz/BST/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.BST;
+
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+        System.out.println("Initiating the Binary Search Tree!!!");
+
+    }
+}
