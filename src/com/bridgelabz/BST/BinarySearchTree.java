7975584e8518d1e8009ec61c5d22fbe00a652a3a
saurav4986 <saurav.raj4986@gmail.com>
2022-11-29 18:07:53 +0530

    Added: codes for creating BST and size method

--- src/com/bridgelabz/BST/BinarySearchTree.java
+++ src/com/bridgelabz/BST/BinarySearchTree.java
@@ -1,34 +1,32 @@
 package com.bridgelabz.BST;
 
-/*(UC-1_Create_BST)
-Ability to create a BST by adding 56 and then
-adding 30 & 70 - Use INode to create My Binary Node
-- Note the key has to extend comparable to compare and determine left or right node
-- First add 56 as root node so 30 will be added to left and 70 to right*/
+/*(UC-2_Add_size)
+Ability to create the binary tree shown in the figure
+- Check if all are added with using size method in Binary Tree*/
 
 public class BinarySearchTree {
     private TreeNode root;
 
-    private class TreeNode{
+    private class TreeNode {
         private int data;
         private TreeNode left;
         private TreeNode right;
 
-        public TreeNode(int data){
+        public TreeNode(int data) {
             this.data = data;
         }
     }
 
-    public void insert(int value){
+    public void insert(int value) {
         root = insert(root, value);
     }
 
-    public TreeNode insert( TreeNode root, int value){
-        if (root == null || root.data == value){
+    public TreeNode insert(TreeNode root, int value) {
+        if (root == null || root.data == value) {
             root = new TreeNode(value);
             return root;
         }
-        if (value < root.data){
+        if (value < root.data) {
             root.left = insert(root.left, value);
         } else {
             root.right = insert(root.right, value);
@@ -36,11 +34,44 @@ public class BinarySearchTree {
         return root;
     }
 
+    public void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    int size() {
+        return size(root);
+    }
+
+    int size(TreeNode root) {
+        if (root == null)
+            return 0;//if tree is empty then return zero
+        else
+            return (size(root.left) + 1 + size(root.right));// 1 to add the root of the binary tree
+        // and traversing the left and right of the bst
+    }
+
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
-        tree.insert(56);
-        tree.insert(30);
+        tree.insert(56); // root
+        tree.insert(30); // root.left
+        tree.insert(22); // root.right
+        tree.insert(40); // root.right
+        tree.insert(11); // root.right
+        tree.insert(3); // root.right
+        tree.insert(16); // root.right
         tree.insert(70);
-
+        tree.insert(60);
+        tree.insert(95);
+        tree.insert(65);
+        tree.insert(63);
+        tree.insert(67);
+        tree.inOrder(tree.root);// retrieving values in ascending order.
+        System.out.println();
+        System.out.println("Size of the tree is: " + tree.size());
     }
 }
